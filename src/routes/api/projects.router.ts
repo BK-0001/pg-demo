bc442db3ad92637d33b027b078804eed880d8479
Bart Kang <bart.kang.0719@gmail.com>
2024-07-29 11:14:00 -0700

    implement get projects

--- src/routes/api/projects.router.ts
+++ src/routes/api/projects.router.ts
@@ -1,12 +1,25 @@
 import { Request, Response, Router } from "express";
+import { pool } from "../../db";
 import { router as tasksRouter } from "./tasks.router";
+
+type Project = {
+  id: number;
+  title: string;
+  description: string | null;
+  created_at: Date;
+};
 
 export const router = Router();
 
 router.use("/:projectId/tasks", tasksRouter);
 
-router.get("/", (req: Request, res: Response) => {
-  res.send("");
+// /api/v1/projects
+router.get("/", async (req: Request, res: Response) => {
+  const data = await pool.query<Project>(
+    `SELECT title, description FROM projects;`
+  );
+
+  res.send(data.rows);
 });
 
 router.get("/:id", (req: Request, res: Response) => {
